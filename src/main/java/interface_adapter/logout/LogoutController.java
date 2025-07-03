d41ffa2b149e599a96a619d7401d289e1c448c12
Ruhani Walia <ruhani.walia@mail.utoronto.ca>
2025-07-03 18:43:07 -0400

    Completed LogoutController

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,25 +1,35 @@
 package interface_adapter.logout;
 
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutInputData;
 
 /**
  * The controller for the Logout Use Case.
  */
 public class LogoutController {
 
-    private LogoutInputBoundary logoutUseCaseInteractor;
+    // the instance variable
+    private final LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        // receiving an object (logoutUseCaseInteractor) that implements the logout logic
+        // need to store this object in the class to use later (like in the execute method)
+        // do so by saving the interactor in the instance variable
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
      * Executes the Logout Use Case.
      * @param username the username of the user logging in
      */
+    // this method gets called when the user clicks "Log Out"
+    // need to create a LogoutInputData object with the username
+    // also need to tell the interactor to execute using that input
     public void execute(String username) {
-        // TODO: run the use case interactor for the logout use case
         // 1. instantiate the `LogoutInputData`, which should contain the username.
+        final LogoutInputData inputData = new LogoutInputData(username);
         // 2. tell the Interactor to execute.
+        logoutUseCaseInteractor.execute(inputData);
+        // this runs the use case interactor for the logout use case
     }
 }
